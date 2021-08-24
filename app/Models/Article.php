b26c8a59a62cf9362628838250628868a0b12e84
Abah Faqih <rasyidperkim@gmail.com>
2021-08-24 23:40:07 +0800

    belongsto

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -7,5 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
+    protected $guarded = [];
     use HasFactory;
+    
+    /**
+     * Get the user that owns the Article
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
 }
